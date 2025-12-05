e0f76ae272d39b04bcd199dbe6d4a8e86405187c
Nhatanh5237 <duongtrannhatanh4@gmail.com>
2025-12-05 11:15:16 +0700

    feat buoi2 php

--- SQL_PHP/buoi2/login.php
+++ SQL_PHP/buoi2/login.php
@@ -4,10 +4,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Buổi 2 php</title>
+    <title>Buổi 2</title>
+
+    <style>
+        .warning {
+            color: red;
+        }
+    </style>
 </head>
 
 <body>
+
     <form action="login.php" method="post">
         <h1>Đăng nhập</h1>
         <div>
@@ -17,29 +24,36 @@
             <input type="password" name="password" placeholder="Mật khẩu">
         </div>
         <div>
-            <input type="submit" value="Đăng nhập">
+            <input type="submit" value="Đăng Nhập">
         </div>
-
     </form>
     <?php
+    include("connect.php");
     if (isset($_POST['username']) && isset($_POST['password'])) {
+
+
         $tenDangNhap = $_POST['username'];
         $matKhau = $_POST['password'];
 
-        //Nếu tên đăng nhập = admin
-        //Mật khẩu 123 thì cho phép người dùng vào trang chủ
-    
-        //echo $tenDangNhap . $matKhau;
-    
-        if ($tenDangNhap == 'admin' && $matKhau == '123') {
+
+        $sql = "select * from nguoi_dung where ten_dang_nhap = '$tenDangNhap' and mat_khau = '$matKhau'";
+
+        $result = mysqli_query($conn, $sql);
+
+
+        if (mysqli_num_rows($result) > 0) {
             session_start();
-            $_SESSION['username'] = $tenDangNhap;
-            header('location: trangchu.php');
+            $_SESSION["username"] = $tenDangNhap;
+
+            header('location: index.php');
+            
         } else {
-            header('location:trangchu.php');
+            echo "<p class='warning'>Sai thông tin đăng nhập</p>";
         }
     }
     ?>
+
+
 </body>
 
 </html>
